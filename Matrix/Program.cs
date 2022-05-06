135a68c91eb2a34085ea3216aa9250c71539a9a7
Gleb <gleb.gadbrat@mail.ru>
2022-05-06 11:41:37 +0300

    fixed Matrix

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -15,7 +15,7 @@ namespace Matrix
                     }
                 }
             }
-            void sumElem(int[,] Mat) // Задание 1
+            int sumElem(int[,] Mat) // Задание 1
             {
                 int sum = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
@@ -25,9 +25,9 @@ namespace Matrix
                         sum += Mat[i,j];
                     }
                 }
-                Console.WriteLine($"Сумма равна {sum}");
+                return (sum);
             }
-            void proizvElem(int[,] Mat) // Задание 2
+            float proizvElem(int[,] Mat) // Задание 2
             {
                 int proizv = 1;
                 for (int i = 0; i < Mat.GetLength(0); i++)
@@ -37,9 +37,9 @@ namespace Matrix
                         proizv *= Mat[i,j];
                     }
                 }
-                Console.WriteLine($"Произведение равно {proizv}");
+                return (proizv);
             }
-            void maxElem(int[,] Mat) //Задание 3
+            int maxElem(int[,] Mat) //Задание 3
             {
                 int max = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
@@ -49,9 +49,9 @@ namespace Matrix
                         if (Mat[i,j] > max) max = Mat[i,j];
                     }
                 }
-                Console.WriteLine($"Максимальное число равно {max}");
+                return (max);
             }
-            void minElem(int[,] Mat) //Задание 4
+            int minElem(int[,] Mat) //Задание 4
             {
                 int min = 100000;
                 for (int i = 0; i < Mat.GetLength(0); i++)
@@ -61,81 +61,81 @@ namespace Matrix
                         if (Mat[i,j] < min) min = Mat[i,j];
                     }
                 }
-                Console.WriteLine($"Минимальное число равно {min}");
+                return (min);
             }
-            void sumDiag(int[,] Mat) //Задание 5
+            int sumDiag(int[,] Mat) //Задание 5
             {
                 int sum = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     sum += Mat[i,i];
                 }
-                Console.WriteLine($"Сумма главной диагонали равна {sum}");
+                return (sum);
             }
-            void proizvDiag(int[,] Mat) //Задание 6
+            int proizvDiag(int[,] Mat) //Задание 6
             {
                 int proizv = 1;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     proizv *= Mat[i,i];
                 }
-                Console.WriteLine($"Произведение  на главной диагонале равно {proizv}");
+                return (proizv);
             }
-            void maxDiag(int[,] Mat) //Задание 7
+            int maxDiag(int[,] Mat) //Задание 7
             {
                 int max = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     if (Mat[i,i] > max) max = Mat[i,i];
                 }
-                Console.WriteLine($"Максимальное число на диагонале равно {max}");
+                return (max);
             }
-            void minDiag(int[,] Mat) //Задание 8
+            int minDiag(int[,] Mat) //Задание 8
             {
                 int min = 10000000;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     if (Mat[i,i] < min) min = Mat[i,i];
                 }
-                Console.WriteLine($"Минимальное число на диагонале равно {min}");
+                return (min);
             }
-            void sumPobDiag(int[,] Mat) //Задание 9
+            int sumPobDiag(int[,] Mat) //Задание 9
             {
                 int sum = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     sum += Mat[i, Mat.GetLength(0) - i - 1];
                 }
-                Console.WriteLine($"Сумма побочной диагонали равна {sum}");
+                return (sum);
             }
-            void proizvPobDiag(int[,] Mat) //Задание 10
+            int proizvPobDiag(int[,] Mat) //Задание 10
             {
                 int proizv = 1;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     proizv *= Mat[i, Mat.GetLength(0) - i - 1];
                 }
-                Console.WriteLine($"Произведение побочной диагонали равно {proizv}");
+                return (proizv);
             }
-            void maxPobDiag(int[,] Mat) //Задание 11
+            int maxPobDiag(int[,] Mat) //Задание 11
             {
                 int max = 0;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     if (Mat[i, Mat.GetLength(0) - i - 1] > max) max = Mat[i, Mat.GetLength(0) - i - 1];
                 }
-                Console.WriteLine($"Максимальное число побочной диагонали равно {max}");
+                return (max);
             }
-            void minPobDiag(int[,] Mat) //Задание 12
+            int minPobDiag(int[,] Mat) //Задание 12
             {
                 int min = 100000000;
                 for (int i = 0; i < Mat.GetLength(0); i++)
                 {
                     if (Mat[i, Mat.GetLength(0) - i - 1] < min) min = Mat[i, Mat.GetLength(0) - i - 1];
                 }
-                Console.WriteLine($"Минимальное число побочной диагонали равно {min}");
+                return (min);
             }
-            void minMaxElem(int[,] Mat) //Задание 13
+            int minMaxElem(int[,] Mat) //Задание 13
             {
                 int max = 0;
                 int minMax = 10000000;
@@ -148,7 +148,7 @@ namespace Matrix
                     if (max < minMax) minMax = max;
                     max = 0;
                 }
-                Console.WriteLine($"Минимальное число из максимальных равно {minMax}");
+                return (minMax);
             }
             void transMat (int[,] Mat) //Задание 14
             {
@@ -160,7 +160,6 @@ namespace Matrix
                         transmat[i,j] = Mat[j,i];
                     }
                 }
-                Console.WriteLine("Транспонированная матрица: ");
                 printMat(transmat);
             }
             void sumMats (int[,] Mat1, int[,] Mat2) //Задание 15
@@ -173,7 +172,6 @@ namespace Matrix
                         matsum[i,j] = Mat1[i,j] + Mat2[i,j];
                     }
                 }
-                Console.WriteLine("Сумма матриц равна:");
                 printMat(matsum);
             }
             void printMat (int[,] Mat) //Задание 16
@@ -200,20 +198,35 @@ namespace Matrix
             printMat(mat);
             fillMat(mat2);
             printMat(mat2);
-            sumElem(mat);
-            proizvElem(mat);
-            maxElem(mat);
-            minElem(mat);
-            sumDiag(mat);
-            proizvDiag(mat);
-            maxDiag(mat);
-            minDiag(mat);
-            sumPobDiag(mat);
-            proizvPobDiag(mat);
-            maxPobDiag(mat);
-            minPobDiag(mat);
-            minMaxElem(mat);
+            Console.Write("Сумма равна ");
+            Console.WriteLine(sumElem(mat));
+            Console.Write($"Произведение равно ");
+            Console.WriteLine(proizvElem(mat));
+            Console.Write($"Максимальное число равно ");
+            Console.WriteLine(maxElem(mat));
+            Console.Write($"Минимальное число равно ");
+            Console.WriteLine(minElem(mat));
+            Console.Write($"Сумма главной диагонали равна ");
+            Console.WriteLine(sumDiag(mat));
+            Console.Write($"Произведение  на главной диагонале равно ");
+            Console.WriteLine(proizvDiag(mat));
+            Console.Write($"Максимальное число на диагонале равно ");
+            Console.WriteLine(maxDiag(mat));
+            Console.Write($"Минимальное число на диагонале равно ");
+            Console.Write(minDiag(mat));
+            Console.Write($"Сумма побочной диагонали равна ");
+            Console.WriteLine(sumPobDiag(mat));
+            Console.Write($"Произведение побочной диагонали равно ");
+            Console.WriteLine(proizvPobDiag(mat));
+            Console.Write($"Максимальное число побочной диагонали равно ");
+            Console.WriteLine(maxPobDiag(mat));
+            Console.Write($"Минимальное число побочной диагонали равно ");
+            Console.WriteLine(minPobDiag(mat));
+            Console.Write($"Минимальное число из максимальных равно ");
+            Console.WriteLine(minMaxElem(mat));
+            Console.WriteLine("Транспонированная матрица:");
             transMat(mat);
+            Console.WriteLine("Сумма матриц равна:");
             sumMats(mat, mat2);
 
         }
